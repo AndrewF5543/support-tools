9ef51a3feee945cee078a3cabd0479eec3df6993
Nathan Cooprider <nathan.cooprider@threatstack.com>
2017-04-19 14:33:34 -0400

    A place for public debugging scripts, and the first of the scripts.
    https://threatstack.atlassian.net/browse/ITR-318

---
+++ docker/gather_docker_info.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Copyright (c) 2017 All Rights Reserved, http://www.threatstack.com
+echo "/* Gathering docker information ****************************************/"
+if ! hash docker 2>/dev/null; then
+    echo "Unable to find docker!"
+    exit
+fi
+
+echo "/* docker version ******************************************************/"
+docker version
+echo "/* docker info *********************************************************/"
+docker info
+echo "/* ls -l /var/lib/docker/containers ************************************/"
+ls -l /var/lib/docker/containers
+echo "/* cat /var/lib/docker/containers/*/config* ****************************/"
+cat /var/lib/docker/containers/*/config*
+echo "/* docker ps ***********************************************************/"
+docker ps
+
+echo "/* Get a container ID **************************************************/"
+CID=`docker ps | tail -n 1 | awk '{print $1}'`
+echo $CID
+echo "/* Docker json *********************************************************/"
+echo -e "GET /containers/${CID}/json HTTP/1.1\r\nHost:\r\n" | nc -U /var/run/docker.sock
+echo "/* Docker top **********************************************************/"
+echo -e "GET /containers/${CID}/top HTTP/1.1\r\nHost:\r\n" | nc -U /var/run/docker.sock
+
+echo "/* Get a process ID ****************************************************/"
+PID=`echo -e "GET /containers/${CID}/top HTTP/1.1\r\nHost:\r\n" | nc -U /var/run/docker.sock | tail -1 | sed 's/^.*\[\[\([^]]*\)\]\].*/\1/' | awk -F',' '{ print $2 }' | tr -d '"'`
+echo $PID
+echo "/* readlink ************************************************************/"
+readlink /proc/${PID}/ns/pid
+
+echo "/* Done gathering docker information ***********************************/"
