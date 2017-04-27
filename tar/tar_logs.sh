af143f599806d85adb25defb02b139943ee3491b
Andrew Foster <andrewfoster@Andrew-Fosters-MacBook-Pro.local>
2017-04-27 16:00:02 -0400

    Adding in an extra underscore for clarity

--- tar/tar_logs.sh
+++ tar/tar_logs.sh
@@ -3,5 +3,5 @@
 # Copyright (c) 2017 All Rights Reserved, http://www.threatstack.com
 echo "/* Tarring log files ***************************************************/"
 current_date="$(date +'%m-%d-%Y')"
-tar -cvzf ts_logs$current_date.tar.gz /opt/threatstack/cloudsight/logs/ || true
+tar -cvzf ts_logs_$current_date.tar.gz /opt/threatstack/cloudsight/logs/ || true
 echo "/* Done tarring log files **********************************************/"
