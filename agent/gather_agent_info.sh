ca4522512d53371b7e2c9524615eca2092606e55
Andrew Foster <andrewfoster@Andrew-Fosters-MacBook-Pro.local>
2017-09-14 09:54:59 -0400

    Removing the DEBUG cloudsight step

--- agent/gather_agent_info.sh
+++ agent/gather_agent_info.sh
@@ -7,11 +7,6 @@ echo "/* cloudsight version **************************************************/"
 /opt/threatstack/bin/cloudsight version
 echo "/* cloudsight status ***************************************************/"
 /opt/threatstack/bin/cloudsight status
-echo "/* debug cloudsight status *********************************************/"
-DEBUG=cloudsight /opt/threatstack/bin/cloudsight status 2>&1
-# Make doubly sure we're back to non-debug
-unset DEBUG
-/opt/threatstack/bin/cloudsight status > /dev/null 2>&1
 if hash dpkg-query 2>/dev/null; then
     echo "/* dpkg-query -l | grep threatstack ********************************/"
     dpkg-query -l | grep threatstack
