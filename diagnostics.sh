a3be4feb8b5eb1cb685e5fd8c17a382917a51a25
Ubuntu <ubuntu@ip-172-31-72-118.ec2.internal>
2017-10-24 19:14:59 +0000

    diagnostics.sh now encrypts by default (override with --send-unsafe)

--- diagnostics.sh
+++ diagnostics.sh
@@ -11,13 +11,22 @@ bash system/gather_system_info.sh | sudo tee /tmp/threatstack_diagnostics/system
 bash docker/gather_docker_info.sh | sudo tee /tmp/threatstack_diagnostics/docker_info.txt
 bash agent/gather_agent_info.sh | sudo tee /tmp/threatstack_diagnostics/agent_info.txt
 bash agent/gather_fim_info.sh | sudo tee /tmp/threatstack_diagnostics/fim_info.txt
-echo "/* COPYING THREAT STACK LOG FILES *** **********************************/"
+echo "/* Copying threat stack log files **************************************/"
 cp /opt/threatstack/cloudsight/logs/cloudsight.log /tmp/threatstack_diagnostics/cloudsight.log
 cp /opt/threatstack/cloudsight/logs/install.log /tmp/threatstack_diagnostics/install.log
 cp /opt/threatstack/cloudsight/logs/error.log /tmp/threatstack_diagnostics/error.log
 cp /opt/threatstack/cloudsight/logs/threatstack-audit.log /tmp/threatstack_diagnostics/threatstack-audit.log
 cp /opt/threatstack/cloudsight/logs/threatstack-tsfim.log /tmp/threatstack_diagnostics/threatstack-tsfim.log
-bash tar/tar_logs.sh
-rm -rf /tmp/threatstack_diagnostics
+echo "/* Tarring log files ***************************************************/"
+current_date="$(date +'%m-%d-%Y')"
+name=ts_logs_$(hostname)_${current_date}.tar.gz
+( bash tar/tar_logs.sh $name ) || exit 1
+echo "/* Done tarring log files **********************************************/"
+security_setting=${1:-"--send-safe"}
+if [ $security_setting != "--send-unsafe"  ] ; then
+    gpg --import support.pub
+    gpg --encrypt --trust-model always -r support@threatstack.com $name
+fi
+rm -rf /tmp/threatstack_diagnostics 
 echo "/* ENDING THREAT STACK DIAGNOSTICS *************************************/"
 
--- tar/tar_logs.sh
+++ tar/tar_logs.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Copyright (c) 2017 All Rights Reserved, http://www.threatstack.com
-echo "/* Tarring log files ***************************************************/"
-current_date="$(date +'%m-%d-%Y')"
-name=ts_logs_$(hostname)_${current_date}
-tar -czvf ${name}.tar.gz -C /tmp/ threatstack_diagnostics/*
-echo "/* Done tarring log files **********************************************/"
+#current_date="$(date +'%m-%d-%Y')"
+#name=ts_logs_$(hostname)_${current_date}
+#tar -czvf ${name}.tar.gz -C /tmp/ threatstack_diagnostics || ( echo "Failed to generate tarball! Exiting" ; exit 1 )
+tar -czvf $1 -C /tmp/ threatstack_diagnostics || ( echo "Failed to generate tarball! Exiting" ; exit 1 )
